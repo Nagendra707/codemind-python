dfc9d7f307b47b444453695eecf39e62496b56cc
Nagendra707 <111426822+Nagendra707@users.noreply.github.com>
2022-09-06 15:46:40 +0530

    a=int(input()) b=int(input()) a1=0 for i in range(1,a):     if a%i==0:         a1+=i b1=0 for i in range(1,b):     if b%i==0:         b1+=i if a1==b and b1==a:     print("Amicable") else:     print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=int(input())
+a1=0
+for i in range(1,a):
+    if a%i==0:
+        a1+=i
+b1=0
+for i in range(1,b):
+    if b%i==0:
+        b1+=i
+if a1==b and b1==a:
+    print("Amicable")
+else:
+    print("Not Amicable")
