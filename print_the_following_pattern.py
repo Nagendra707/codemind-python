0b9cc2a4d9b43f45de4de3a82a8aa4e89b23b569
Nagendra707 <111426822+Nagendra707@users.noreply.github.com>
2022-10-27 12:57:47 +0530

    n=int(input()) for i in range(1,n+1):     l=n*["x"]     l.insert(i,"0")     l.remove(l[i-1])     c=""     for j in l:         c+=j     print(c)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,9 @@
 n=int(input())
-for i in range(n,0,-1):
-    for j in range(n,0,-1):
-        print(j,end=" ")
-    print()
+for i in range(1,n+1):
+    l=n*["x"]
+    l.insert(i,"0")
+    l.remove(l[i-1])
+    c=""
+    for j in l:
+        c+=j
+    print(c)
