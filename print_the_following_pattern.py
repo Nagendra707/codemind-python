1639320bca376229b263e4af32004715944765b7
Nagendra707 <111426822+Nagendra707@users.noreply.github.com>
2022-10-27 12:59:09 +0530

    n=int(input()) l="ABCDEFGHIJKLMNOPQRSTUVWXYZ" l=list(l) for i in range(n):     for j in range(n):         print(l[i],end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,7 @@
 n=int(input())
-for i in range(1,n+1):
-    l=n*["x"]
-    l.insert(i,"0")
-    l.remove(l[i-1])
-    c=""
-    for j in l:
-        c+=j
-    print(c)
+l="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+l=list(l)
+for i in range(n):
+    for j in range(n):
+        print(l[i],end=" ")
+    print()
